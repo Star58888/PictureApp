8becee4d26005183d7f7f919f9a0d7b9d440c2bd
Star58888 <s0913940588@gmail.com>
2017-11-19 16:19:27 +0800

    Initial commit

---
+++ app/src/main/java/com/star/pictureapp/DetailActivity.java
@@ -0,0 +1,92 @@
+package com.star.pictureapp;
+
+import android.content.CursorLoader;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+public class DetailActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
+    GestureDetector detector;
+    private int position;
+    private ImageView img;
+    private Cursor cursor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        detector = new GestureDetector(this , this);
+        position = getIntent().getIntExtra("POSITION" , 0);
+        img = (ImageView) findViewById(R.id.imageView);
+        CursorLoader loader = new CursorLoader(this , MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,null,null,null);
+        cursor = loader.loadInBackground();
+        cursor.moveToPosition(position);
+        updateImage();
+    }
+
+    private void updateImage() {
+        String imagePath = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+        img.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        float distance = e2.getX() - e1.getX();
+        if (distance > 100)
+        //往右，往前一張圖
+        {
+            if (!cursor.moveToPrevious())
+            {
+                cursor.moveToLast();
+            }
+            updateImage();
+        }
+       else if (distance < -100)
+       //往左，往後一張圖
+        {
+            if (!cursor.moveToNext())
+            {
+                cursor.moveToFirst();
+            }
+            updateImage();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event);
+    }
+}
